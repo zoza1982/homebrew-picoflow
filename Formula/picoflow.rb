9143f2a181741a5c362105e0b74978b532a672b3
Zoran Vukmirica <11164593+zoza1982@users.noreply.github.com>
2026-01-29 22:38:55 -0600

    Update picoflow to v0.1.0

--- Formula/picoflow.rb
+++ Formula/picoflow.rb
@@ -6,11 +6,11 @@ class Picoflow < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-darwin-arm64-macos.tar.gz"
+      sha256 "5fa096b16929decd1280d942018c1a3f0c32743cdbb8c7d7e272575c15784ac7"
     else
-      url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-darwin-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_X86_64"
+      url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-darwin-x86_64-macos.tar.gz"
+      sha256 "13e9d1ad981acef91993eed306e789ee4c2dc5f8a1229968edb4cb5988b4f5dd"
     end
   end
 
@@ -18,14 +18,14 @@ class Picoflow < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-arm64-linux.tar.gz"
-        sha256 "PLACEHOLDER_ARM64_LINUX"
+        sha256 "eed9a9402f254f4f64b638133eadbfbf83afd02666f66b1e91d7a1f1e898a6c2"
       else
         url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-arm32-linux.tar.gz"
-        sha256 "PLACEHOLDER_ARM32_LINUX"
+        sha256 "54783f4d2e53e75bdcbca7605976bd83b23bda8963f8dd1fa474e16354618599"
       end
     else
       url "https://github.com/zoza1982/picoflow/releases/download/v0.1.0/picoflow-v0.1.0-x86_64-linux.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_LINUX"
+      sha256 "f8f4dfe295cb05aea9411493945e84021735e8315d201b30fe3c535a337f244d"
     end
   end
 
@@ -37,3 +37,4 @@ class Picoflow < Formula
     assert_match version.to_s, shell_output("#{bin}/picoflow --version")
   end
 end
+
